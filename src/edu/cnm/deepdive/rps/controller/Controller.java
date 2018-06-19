966d32a1c421a0db6db826821445b832c1e3f072
Nick Bennett <nick@nickbenn.com>
2018-06-19 15:16:52 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/rps/controller/Controller.java
@@ -0,0 +1,143 @@
+package edu.cnm.deepdive.rps.controller;
+
+import edu.cnm.deepdive.rps.model.Terrain;
+import edu.cnm.deepdive.rps.view.TerrainView;
+import java.util.Random;
+import java.util.ResourceBundle;
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+  private static final int STEPS_PER_ITERATION = 100;
+  private static final long MAX_SLEEP_PER_ITERATION = 10;
+
+  @FXML
+  private ResourceBundle resources;
+  @FXML
+  private CheckBox fitCheckbox;
+  @FXML
+  private Text iterationsLabel;
+  @FXML
+  private ScrollPane viewScroller;
+  @FXML
+  private TerrainView terrainView;
+  @FXML
+  private Slider speedSlider;
+  @FXML
+  private Slider mixingSlider;
+  @FXML
+  private Button reset;
+  @FXML
+  private Button stop;
+  @FXML
+  private Button start;
+
+  private double defaultViewHeight;
+  private double defaultViewWidth;
+  private double fitViewHeight;
+  private double fitViewWidth;
+  private String iterationFormat;
+  private Terrain terrain;
+  private boolean running = false;
+  private Runner runner = null;
+  private final Object lock = new Object();
+  private Timer timer;
+
+  @FXML
+  private void initialize() {
+    terrain = new Terrain(new Random());
+    defaultViewWidth = terrainView.getWidth();
+    defaultViewHeight = terrainView.getHeight();
+    fitViewWidth = viewScroller.getWidth();
+    fitViewHeight = viewScroller.getHeight();
+    iterationFormat = iterationsLabel.getText();
+    terrainView.setSource(terrain.getCells());
+    draw();
+    timer = new Timer();
+  }
+
+  @FXML
+  private void fitView(ActionEvent actionEvent) {
+    if (fitCheckbox.isSelected()) {
+      terrainView.setWidth(fitViewWidth);
+      terrainView.setHeight(fitViewHeight);
+    } else {
+      terrainView.setWidth(defaultViewWidth);
+      terrainView.setHeight(defaultViewHeight);
+    }
+    if (!running) {
+      draw();
+    }
+  }
+
+  @FXML
+  private void start(ActionEvent actionEvent) {
+    running = true;
+    start.setDisable(true);
+    stop.setDisable(false);
+    reset.setDisable(true);
+    timer.start();
+    runner = new Runner();
+    runner.start();
+  }
+
+  @FXML
+  private void stop(ActionEvent actionEvent) {
+    running = false;
+    runner = null;
+    start.setDisable(false);
+    stop.setDisable(true);
+    reset.setDisable(false);
+    timer.stop();
+  }
+
+  @FXML
+  private void reset(ActionEvent actionEvent) {
+    terrain.reset();
+    draw();
+  }
+
+  private void draw() {
+    synchronized (lock) {
+      terrainView.draw();
+      iterationsLabel.setText(String.format(iterationFormat, terrain.getIterations()));
+    }
+  }
+
+  private class Timer extends AnimationTimer {
+
+    @Override
+    public void handle(long now) {
+      draw();
+    }
+
+  }
+
+  private class Runner extends Thread {
+
+    @Override
+    public void run() {
+      while (running) {
+        synchronized (lock) {
+          terrain.iterate(STEPS_PER_ITERATION);
+        }
+        try {
+          Thread.sleep(1 + MAX_SLEEP_PER_ITERATION - (long) speedSlider.getValue());
+        } catch (InterruptedException e) {
+          // DO NOTHING.
+        }
+      }
+    }
+
+  }
+
+}
+
+
---
+++ src/edu/cnm/deepdive/rps/model/Breed.java
@@ -0,0 +1,42 @@
+package edu.cnm.deepdive.rps.model;
+
+import java.util.Random;
+
+public enum Breed {
+  ROCK, PAPER, SCISSORS;
+
+  public Breed play(Breed opponent) {
+    Breed victor = this;
+    switch (this) {
+      case ROCK:
+        if (opponent == PAPER) {
+          victor = opponent;
+        }
+        break;
+      case PAPER:
+        if (opponent == SCISSORS) {
+          victor = opponent;
+        }
+        break;
+      case SCISSORS:
+        if (opponent == ROCK) {
+          victor = opponent;
+        }
+        break;
+    }
+    return victor;
+  }
+
+  public static Breed random(Random rng) {
+    Breed[] choices = values();
+    return choices[rng.nextInt(choices.length)];
+  }
+
+}
+
+
+
+
+
+
+
